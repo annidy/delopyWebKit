b3cab7d616d70a784a2a861b2c0fab24f871373d
冯星 <annidy@gmail.com>
2019-08-28 15:19:42 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# delopyWebKit
+
+快速拷贝WebKit模拟器文件
+
+Usage:
+```
+sudo node deplopyWebKit 'YOUR BUILD DIR'
+```
---
+++ main.js
@@ -0,0 +1,53 @@
+'use strict'
+
+var shell = require('shelljs');
+var fs = require('fs')
+const path = require('path')
+const process = require('process')
+
+
+
+const SourceDir = process.argv[2]
+
+if (SourceDir == undefined) {
+    console.log('set webkit build dir first');
+    return
+}
+
+var LibraryDir = process.argv[3]
+if (LibraryDir == undefined) {
+    LibraryDir = '/Applications/Xcode.app/Contents/Developer/Platforms/iPhoneOS.platform/Developer/Library/CoreSimulator/Profiles/Runtimes/iOS.simruntime/Contents/Resources/RuntimeRoot/System/Library'
+    console.log('unset xcode library path, use default', LibraryDir);
+    
+}
+
+const FrameworksDir = path.join(LibraryDir, 'Frameworks')
+const PrivateFrameworksDir = path.join(LibraryDir, 'PrivateFrameworks')
+
+function safeCopy(dstfra) {
+    var name = path.basename(dstfra)
+    var srcfra = path.join(SourceDir, name)
+    try {
+        fs.statSync(srcfra)
+    } catch (error) {
+        console.log(error);
+        return
+    }
+
+    try {
+        fs.statSync(dstfra+'.bak')
+    } catch (error) {
+        console.log('backup', dstfra);
+        shell.mv(dstfra, dstfra+'.bak')
+    }
+    console.log(`ln -s ${srcfra} ${dstfra}`)
+    shell.ln('-s', srcfra, dstfra)
+}
+
+
+safeCopy(path.join(FrameworksDir, 'JavaScriptCore.framework'))
+safeCopy(path.join(FrameworksDir, 'WebKit.framework'))
+safeCopy(path.join(PrivateFrameworksDir, 'WebCore.framework'))
+safeCopy(path.join(PrivateFrameworksDir, 'WebKitLegacy.framework'))
+
+
